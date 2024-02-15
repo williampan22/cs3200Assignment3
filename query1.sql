7fbac3c29e5aaed9049cc8691f23f00725204575
William Pan <williamlpan22@gmail.com>
2024-02-15 15:40:13 -0800

    update query1.sql

--- query1.sql
+++ query1.sql
@@ -2,7 +2,7 @@
 -- Create a new Table Music Video, that is a class of type Track (generalization) but adds the attribute Video director. 
 -- A music video is a track. There cannot be a video without a track, and each track can have either no video or just one. 
 
-CREATE TABLE IF NOT EXISTS "music_video" (
+CREATE TABLE "music_video" (
     "TrackId"	INTEGER NOT NULL,
 	"VideoDirector"	TEXT NOT NULL,
 	PRIMARY KEY("TrackID"),
