88ce987b741e7315a407fa6c2afdd865ddff15c1
William Pan <williamlpan22@gmail.com>
2024-02-15 15:26:38 -0800

    Fixed SQL

--- query3.sql
+++ query3.sql
@@ -2,7 +2,7 @@
 -- Insert another video for the track "Voodoo", assuming that you don't know the TrackId, 
 -- so your insert query should specify the TrackId directly.
 
-INSERT INTO music_video (trackId, videoDirector)
+INSERT INTO music_video (TrackId, VideoDirector)
 SELECT TrackId, 'John Alexis Guerra Gomez'
 FROM tracks
 WHERE Name = 'Voodoo';
